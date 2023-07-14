8c9245d571386b131a11ce18ea1a70a83a2abda5
JCCRUZCHACON <confeccionesmc2018@gmail.com>
2023-07-14 10:54:51 -0500

    E-COMERCE

--- src/main.js
+++ src/main.js
@@ -15,13 +15,13 @@ async function getProducts() {
   }
 }
 
-function validateAmountProduct(store,id) {
-  if(store.cart[id].amount === store.cart[id].quantity) {
+function validateAmountProduct(store, id) {
+  if (store.cart[id].amount === store.cart[id].quantity) {
     alert("ya no hay mas en stock");
-  }else{
+  } else {
     store.cart[id].amount++;
   }
-  }
+}
 
 function printProducts(store) {
   let html = "";
@@ -38,8 +38,8 @@ function printProducts(store) {
         <p>$${price}.0- ${quantity} unidades</p>
         ${
           quantity
-          ? `<button class="product__btn" id="${id}">agregar</button>`
-          :"<div></div>"
+            ? `<button class="product__btn" id="${id}">agregar</button>`
+            : "<div></div>"
         }
 
         </div>
@@ -54,15 +54,12 @@ function handleShowCart() {
   const iconCart = document.querySelector(".icon__cart");
   const cart = document.querySelector(".cart");
 
-  iconCart.addEventListener("click", function() {
-  cart.classList.toggle("cart__show");
-    
+  iconCart.addEventListener("click", function () {
+    cart.classList.toggle("cart__show");
   });
-
 }
 
 function printProductsInCart(store) {
-  
   let html = "";
 
   for (const key in store.cart) {
@@ -78,9 +75,7 @@ function printProductsInCart(store) {
       <b>${name}</b>
     </p>
     <p> 
-      <small>price: $${price} | <b>$${
-        amount * price
-      }</b></small>
+      <small>price: $${price} | <b>$${amount * price}</b></small>
           </p>
           <p>
           <small>disponible ${quantity}</small>
@@ -95,117 +90,127 @@ function printProductsInCart(store) {
     </div>
     </div>
     `;
-    
-    };
-    document.querySelector(".cart__products").innerHTML = html;
+  }
+  document.querySelector(".cart__products").innerHTML = html;
 }
 
 function addToCartFromProducts(store) {
   const productsHTML = document.querySelector(".products");
 
-  productsHTML.addEventListener("click", function(e){
+  productsHTML.addEventListener("click", function (e) {
     //TARGET: hace referenecia al elemento que se hace click
-    if((e.target.classList.contains("product__btn"))) {
+    if (e.target.classList.contains("product__btn")) {
       const id = Number(e.target.id);
-      const productFound = store.products.find(function(product){
+      const productFound = store.products.find(function (product) {
         return product.id === id;
       });
 
       //objeto store.car
-      if (store.cart[productFound.id]){
-        validateAmountProduct(store,productFound.id);
-      }else{
-        store.cart[productFound.id]= {
+      if (store.cart[productFound.id]) {
+        validateAmountProduct(store, productFound.id);
+      } else {
+        store.cart[productFound.id] = {
           ...productFound,
           amount: 1,
         };
-      };
+      }
 
       localStorage.setItem("cart", JSON.stringify(store.cart));
       printProductsInCart(store);
       printTotal(store);
 
-    //CURRENTTARGET: hace referencia al elemento que se asigno el evento
-    // console.log(e.currentTarget);
-      };
+      //CURRENTTARGET: hace referencia al elemento que se asigno el evento
+      // console.log(e.currentTarget);
+    }
   });
-
 }
 
 function printTotal(store) {
   let totalProducts = 0;
   let totalPrice = 0;
 
-   for (const key in store.cart) {
-        const { amount, price } = store.cart[key];
+  for (const key in store.cart) {
+    const { amount, price } = store.cart[key];
 
-        totalProducts += amount;
-        totalPrice += amount * price;
-    }
-    document.querySelector("#totalProducts").textContent = totalProducts; 
-    document.querySelector("#totalPrice").textContent = totalPrice;
-    document.querySelector(".ball").textContent = totalProducts;
-
-   }
-
-
-function handleCart(store) {
-  document.querySelector(".cart__products").addEventListener("click", function(e) {
-    if(e.target.classList.contains("bx")) {
-      const id = Number(e.target.parentElement.id);
-        
-      if(e.target.classList.contains("bx-minus")){
-        if(store.cart[id].amount === 1) {
-          const response = confirm("seguro quieres eliminar?");
-          if(response) delete store.cart[id];
-        }else{
-          store.cart[id].amount--;
-        }
-      }
-      if(e.target.classList.contains("bx-plus")){
-        validateAmountProduct(store,id);
-      }
-      if(e.target.classList.contains("bxs-trash")){
-        const response = confirm("seguro quieres eliminar?");
-        if(response) delete store.cart[id];
-        }
-      localStorage.setItem("cart", JSON.stringify(store.cart));
-      printProductsInCart(store);
-      printTotal(store);
-      }
-  });
+    totalProducts += amount;
+    totalPrice += amount * price;
+  }
+  document.querySelector("#totalProducts").textContent = totalProducts;
+  document.querySelector("#totalPrice").textContent = totalPrice;
+  document.querySelector(".ball").textContent = totalProducts;
 }
 
+function handleCart(store) {
+  document
+    .querySelector(".cart__products")
+    .addEventListener("click", function (e) {
+      if (e.target.classList.contains("bx")) {
+        const id = Number(e.target.parentElement.id);
+
+        if (e.target.classList.contains("bx-minus")) {
+          if (store.cart[id].amount === 1) {
+            const response = confirm("seguro quieres eliminar?");
+            if (response) delete store.cart[id];
+          } else {
+            store.cart[id].amount--;
+          }
+        }
+        if (e.target.classList.contains("bx-plus")) {
+          validateAmountProduct(store, id);
+        }
+        if (e.target.classList.contains("bxs-trash")) {
+          const response = confirm("seguro quieres eliminar?");
+          if (response) delete store.cart[id];
+        }
+        localStorage.setItem("cart", JSON.stringify(store.cart));
+        printProductsInCart(store);
+        printTotal(store);
+      }
+    });
+}
 
 function handleTotal(store) {
-  document.querySelector(".btn__buy").addEventListener("click", function() {
-    if(!Object.values(store.cart).length)  return alert("y si primero compras algo?");
+  document.querySelector(".btn__buy").addEventListener("click", function () {
+    if (!Object.values(store.cart).length)
+      return alert("y si primero compras algo?");
     const response = confirm("seguro que quieres comprar?");
-      if(!response) return;
+    if (!response) return;
 
-      const newArray = [];
+    const newArray = [];
 
-      store.products.forEach((product)=> {
-        if(store.cart[product.id]) {
-          newArray.push({
-            ...product,
-            quantity: product.quantity - store.cart[product.id].amount,
-          });
-    }else{
-      newArray.push(product);
-    }
+    store.products.forEach((product) => {
+      if (store.cart[product.id]) {
+        newArray.push({
+          ...product,
+          quantity: product.quantity - store.cart[product.id].amount,
+        });
+      } else {
+        newArray.push(product);
+      }
     });
-  
-  
+
     store.products = newArray;
     store.cart = {};
     localStorage.setItem("products", JSON.stringify(store.products));
     localStorage.setItem("cart", JSON.stringify(store.cart));
 
     printProducts(store);
-    printProductsInCart(store)
+    printProductsInCart(store);
     printTotal(store);
+  });
+}
 
+function loading() {
+  window.addEventListener("load", function () {
+    document.getElementById("loader").classList.toggle("loader2");
+  });
+}
+
+function darkMode() {
+  const switchButton = document.getElementById("switch");
+  switchButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    switchButton.classList.toggle("active");
   });
 }
 
@@ -213,9 +218,8 @@ function handleTotal(store) {
 async function main() {
   //UN OBJETO STORE(TIENDA)
   const store = {
-    products: 
-        JSON.parse(localStorage.getItem("products")) || 
-        (await getProducts()),
+    products:
+      JSON.parse(localStorage.getItem("products")) || (await getProducts()),
     cart: JSON.parse(localStorage.getItem("cart")) || {},
   };
   printProducts(store);
@@ -225,16 +229,8 @@ async function main() {
   handleCart(store);
   printTotal(store);
   handleTotal(store);
-  handleMenu();
-
-
-};
+  loading();
+  darkMode();
+}
 
 main();
-
-
-const switchButton = document.getElementById('switch');
-switchButton.addEventListener('click',()=>{
-    document.body.classList.toggle('dark');
-    switchButton.classList.toggle('active')
-})
